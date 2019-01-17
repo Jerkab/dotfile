ef1efd8c7a72b02892e445d53ad42d31fc0e8f30
Jerkab <jpj.sanchez@gmail.com>
2019-01-17 19:10:00 +0100

    Add some basic files
    Changes to be committed:
    	new file:   create-link.sh
    	new file:   dot.emacs

---
+++ create-link.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+DIRBASE=`cd $(dirname $0) && pwd`
+
+function mylink {
+    name=$1;
+    fname=$DIRBASE/$name
+    if [ -f $fname ]; then
+	hname=`echo $name | sed -e s/dot././`
+	if [ -e "$HOME/$hname" ]; then
+	    echo "IGNORE: File $HOME/$hname already exist"
+	else
+	    #ln -s $fname $HOME/$hname
+	    echo "INFO: Create link $HOME/$hname"
+	fi
+    else
+	echo "IGNORE: $fname is not a file."
+    fi
+}
+
+# List dot files
+DOTFILES=`cd $DIRBASE && ls -1 dit.*`
+if [ -z "$DOTFILES" ]; then
+    echo "ERROR: No dot file found"
+    exit 1;
+fi
+
+for f in $DOTFILES; do
+    mylink $f
+done
+
+exit 0
